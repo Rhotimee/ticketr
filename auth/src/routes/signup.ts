33b9e6e28b8be125d110c76fd3e444d2e2395cf6
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2020-08-18 03:40:38 +0100

    Initial project setup

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
+
+const router = express.Router();
+
+const signupValidation = [
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Password must be between 4 and 20 characters"),
+];
+
+router.post(
+  "/api/users/signup",
+  signupValidation,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+    throw new DatabaseConnectionError();
+    console.log("Creating a user...");
+
+    res.send({ email, password });
+  }
+);
+
+export { router as signupRouter };
