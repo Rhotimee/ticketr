b851433cd64abe84bdd9f9d08db730fd9f2d9332
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2020-08-19 03:52:04 +0100

    implement signin, signout and currentuser

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,49 @@
-import express from "express";
+import jwt from "jsonwebtoken";
+import express, { Request, Response } from "express";
+import { authValidation } from "../middlewares/user-input-validation";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.get("/api/users/signin", () => {});
+router.post(
+  "/api/users/signin",
+  authValidation,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    const userJwt = await jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
