ebf3f4aa60bffffdab20b8a13dd54e0d7ec38c23
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2020-08-21 02:14:18 +0100

    implement auth links

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <>
-      <div>Hello {currentUser.email}</div>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </>
   );
