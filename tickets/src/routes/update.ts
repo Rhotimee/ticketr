68710ccee6b25fee0d403ec82daeb44a8b82578d
rotimi <isaroy007@gmail.com>
2020-09-09 02:40:50 +0100

    prevent user from updating a ticket when it is reserved

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -4,6 +4,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@bamita/common";
 import { Ticket } from "../models/ticket";
 import { ticketValidator } from "./new";
@@ -22,6 +23,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
