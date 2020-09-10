7932d143dc301bc072d2c48a520fb0310705ab01
rotimi <isaroy007@gmail.com>
2020-09-10 01:32:14 +0100

    setup order created listener in payments

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCreatedEvent, Subjects } from "@bamita/common";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      version: data.version,
+      userId: data.userId,
+    });
+
+    await order.save();
+
+    msg.ack();
+  }
+}
