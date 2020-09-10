446121ec012cec10eedc80a3d6628c8d77cfe1ef
rotimi <isaroy007@gmail.com>
2020-09-10 02:54:20 +0100

    listen for order cancelled event in payments

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -31,6 +31,10 @@ const orderSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    status: {
+      type: String,
+      required: true,
+    },
   },
   {
     toJSON: {
