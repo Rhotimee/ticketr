575246a67ae60365a36f6cd7578f5428ddbd33db
rotimi <isaroy007@gmail.com>
2020-09-11 21:23:36 +0100

    feat: update client

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,6 @@
 import React from "react";
 import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 
 const TicketShow = ({ ticket }) => {
   const { doRequest, errors } = useRequest({
@@ -8,7 +9,8 @@ const TicketShow = ({ ticket }) => {
     body: {
       ticketId: ticket.id,
     },
-    onSuccess: (order) => console.log(order),
+    onSuccess: (order) =>
+      Router.push("/orders/[orderId]", `/orders/${order.id}`),
   });
 
   return (
@@ -16,7 +18,7 @@ const TicketShow = ({ ticket }) => {
       <h1>{ticket.title}</h1>
       <h4>{ticket.price}</h4>
       {errors}
-      <button className="btn btn-primary" onClick={doRequest}>
+      <button className="btn btn-primary" onClick={() => doRequest()}>
         Purchase
       </button>
     </div>
